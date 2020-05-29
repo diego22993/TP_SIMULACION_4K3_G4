64e191bfe833eccbfdc44e43047ff452708af742
diego nicolas <diegonicolasheredialigorria@gmail.com>
2020-05-29 13:34:49 -0300

    Cambios form

--- tp5/simulacion_tp5/Form1.Designer.cs
+++ tp5/simulacion_tp5/Form1.Designer.cs
@@ -44,6 +44,16 @@
             this.label5 = new System.Windows.Forms.Label();
             this.txtMinutosFin = new System.Windows.Forms.MaskedTextBox();
             this.label6 = new System.Windows.Forms.Label();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.gridTamaño)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridTiempo)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridSimulacion)).BeginInit();
@@ -52,11 +62,17 @@
             // gridTamaño
             // 
             this.gridTamaño.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridTamaño.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
             this.gridTamaño.Location = new System.Drawing.Point(183, 29);
             this.gridTamaño.Name = "gridTamaño";
             this.gridTamaño.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
             this.gridTamaño.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.gridTamaño.Size = new System.Drawing.Size(472, 104);
+            this.gridTamaño.Size = new System.Drawing.Size(348, 104);
             this.gridTamaño.TabIndex = 0;
             // 
             // label1
@@ -94,10 +110,16 @@
             // gridTiempo
             // 
             this.gridTiempo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.gridTiempo.Location = new System.Drawing.Point(661, 29);
+            this.gridTiempo.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9,
+            this.Column10});
+            this.gridTiempo.Location = new System.Drawing.Point(549, 29);
             this.gridTiempo.Name = "gridTiempo";
             this.gridTiempo.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.gridTiempo.Size = new System.Drawing.Size(452, 128);
+            this.gridTiempo.Size = new System.Drawing.Size(370, 128);
             this.gridTiempo.TabIndex = 5;
             // 
             // btnSimular
@@ -185,6 +207,86 @@
             this.label6.TabIndex = 14;
             this.label6.Text = "Minutos de Fin";
             // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "tamaño";
+            this.Column1.HeaderText = "Tamaño";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 60;
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "probabilidad";
+            this.Column2.HeaderText = "P";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 60;
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "acumulada";
+            this.Column3.HeaderText = "PAC";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 60;
+            // 
+            // Column4
+            // 
+            this.Column4.DataPropertyName = "inferior";
+            this.Column4.HeaderText = "INF";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 60;
+            // 
+            // Column5
+            // 
+            this.Column5.DataPropertyName = "superior";
+            this.Column5.HeaderText = "SUP";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.Width = 60;
+            // 
+            // Column6
+            // 
+            this.Column6.DataPropertyName = "tiempo";
+            this.Column6.HeaderText = "TIEMPO (min)";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            this.Column6.Width = 80;
+            // 
+            // Column7
+            // 
+            this.Column7.DataPropertyName = "probabilidad";
+            this.Column7.HeaderText = "P";
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            this.Column7.Width = 60;
+            // 
+            // Column8
+            // 
+            this.Column8.DataPropertyName = "acumulada";
+            this.Column8.HeaderText = "PAC";
+            this.Column8.Name = "Column8";
+            this.Column8.ReadOnly = true;
+            this.Column8.Width = 60;
+            // 
+            // Column9
+            // 
+            this.Column9.DataPropertyName = "inferior";
+            this.Column9.HeaderText = "INF";
+            this.Column9.Name = "Column9";
+            this.Column9.ReadOnly = true;
+            this.Column9.Width = 60;
+            // 
+            // Column10
+            // 
+            this.Column10.DataPropertyName = "superior";
+            this.Column10.HeaderText = "SUP";
+            this.Column10.Name = "Column10";
+            this.Column10.ReadOnly = true;
+            this.Column10.Width = 60;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -234,6 +336,16 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.MaskedTextBox txtMinutosFin;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
     }
 }
 
