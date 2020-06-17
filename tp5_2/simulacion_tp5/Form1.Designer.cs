dc1402265591ad610ad554b7cf89bb2c891adf49
nicodch <nicodch@hotmail.com>
2020-06-17 19:56:38 -0300

    Nuevo TP

---
+++ tp5_2/simulacion_tp5/Form1.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace simulacion_tp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtHoraInicio = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtHoraFin = new System.Windows.Forms.MaskedTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCantidadIteraciones = new System.Windows.Forms.MaskedTextBox();
+            this.cmbCantidadCabinas = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSimular = new System.Windows.Forms.Button();
+            this.gridTiempo = new System.Windows.Forms.DataGridView();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtTiempoCobro = new System.Windows.Forms.MaskedTextBox();
+            this.txtIndiceLlegada = new System.Windows.Forms.MaskedTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gridTamaño = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.gridTiempo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridTamaño)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtHoraInicio
+            // 
+            this.txtHoraInicio.Location = new System.Drawing.Point(167, 134);
+            this.txtHoraInicio.Name = "txtHoraInicio";
+            this.txtHoraInicio.Size = new System.Drawing.Size(91, 22);
+            this.txtHoraInicio.TabIndex = 32;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(66, 137);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(95, 17);
+            this.label7.TabIndex = 31;
+            this.label7.Text = "Hora de Inicio";
+            // 
+            // txtHoraFin
+            // 
+            this.txtHoraFin.Location = new System.Drawing.Point(167, 163);
+            this.txtHoraFin.Margin = new System.Windows.Forms.Padding(4);
+            this.txtHoraFin.Name = "txtHoraFin";
+            this.txtHoraFin.Size = new System.Drawing.Size(91, 22);
+            this.txtHoraFin.TabIndex = 30;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(77, 166);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 17);
+            this.label5.TabIndex = 29;
+            this.label5.Text = "Hora de Fin";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 76);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(121, 17);
+            this.label4.TabIndex = 28;
+            this.label4.Text = "Cabinas de Cobro";
+            // 
+            // txtCantidadIteraciones
+            // 
+            this.txtCantidadIteraciones.Location = new System.Drawing.Point(167, 105);
+            this.txtCantidadIteraciones.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCantidadIteraciones.Name = "txtCantidadIteraciones";
+            this.txtCantidadIteraciones.Size = new System.Drawing.Size(91, 22);
+            this.txtCantidadIteraciones.TabIndex = 27;
+            // 
+            // cmbCantidadCabinas
+            // 
+            this.cmbCantidadCabinas.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCantidadCabinas.FormattingEnabled = true;
+            this.cmbCantidadCabinas.Items.AddRange(new object[] {
+            "1",
+            "2"});
+            this.cmbCantidadCabinas.Location = new System.Drawing.Point(167, 73);
+            this.cmbCantidadCabinas.Margin = new System.Windows.Forms.Padding(4);
+            this.cmbCantidadCabinas.Name = "cmbCantidadCabinas";
+            this.cmbCantidadCabinas.Size = new System.Drawing.Size(91, 24);
+            this.cmbCantidadCabinas.TabIndex = 26;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(2, 108);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(157, 17);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Cantidad de Iteraciones";
+            // 
+            // btnSimular
+            // 
+            this.btnSimular.Location = new System.Drawing.Point(52, 244);
+            this.btnSimular.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSimular.Name = "btnSimular";
+            this.btnSimular.Size = new System.Drawing.Size(173, 28);
+            this.btnSimular.TabIndex = 24;
+            this.btnSimular.Text = "Simular";
+            this.btnSimular.UseVisualStyleBackColor = true;
+            this.btnSimular.Click += new System.EventHandler(this.btnSimular_Click);
+            // 
+            // gridTiempo
+            // 
+            this.gridTiempo.AllowUserToAddRows = false;
+            this.gridTiempo.AllowUserToDeleteRows = false;
+            this.gridTiempo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridTiempo.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9,
+            this.Column10});
+            this.gridTiempo.Location = new System.Drawing.Point(317, 132);
+            this.gridTiempo.Margin = new System.Windows.Forms.Padding(4);
+            this.gridTiempo.Name = "gridTiempo";
+            this.gridTiempo.RowHeadersWidth = 51;
+            this.gridTiempo.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.gridTiempo.Size = new System.Drawing.Size(501, 140);
+            this.gridTiempo.TabIndex = 23;
+            // 
+            // Column6
+            // 
+            this.Column6.DataPropertyName = "minutos";
+            this.Column6.HeaderText = "Tiempo(min)";
+            this.Column6.MinimumWidth = 6;
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            this.Column6.Width = 80;
+            // 
+            // Column7
+            // 
+            this.Column7.DataPropertyName = "probabilidad";
+            this.Column7.HeaderText = "P";
+            this.Column7.MinimumWidth = 6;
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 60;
+            // 
+            // Column8
+            // 
+            this.Column8.DataPropertyName = "acumulada";
+            this.Column8.HeaderText = "PAC";
+            this.Column8.MinimumWidth = 6;
+            this.Column8.Name = "Column8";
+            this.Column8.ReadOnly = true;
+            this.Column8.Width = 60;
+            // 
+            // Column9
+            // 
+            this.Column9.DataPropertyName = "inferior";
+            this.Column9.HeaderText = "INF";
+            this.Column9.MinimumWidth = 6;
+            this.Column9.Name = "Column9";
+            this.Column9.ReadOnly = true;
+            this.Column9.Width = 60;
+            // 
+            // Column10
+            // 
+            this.Column10.DataPropertyName = "superior";
+            this.Column10.HeaderText = "SUP";
+            this.Column10.MinimumWidth = 6;
+            this.Column10.Name = "Column10";
+            this.Column10.ReadOnly = true;
+            this.Column10.Width = 60;
+            // 
+            // txtTiempoCobro
+            // 
+            this.txtTiempoCobro.Location = new System.Drawing.Point(167, 43);
+            this.txtTiempoCobro.Margin = new System.Windows.Forms.Padding(4);
+            this.txtTiempoCobro.Name = "txtTiempoCobro";
+            this.txtTiempoCobro.Size = new System.Drawing.Size(91, 22);
+            this.txtTiempoCobro.TabIndex = 22;
+            // 
+            // txtIndiceLlegada
+            // 
+            this.txtIndiceLlegada.Location = new System.Drawing.Point(167, 13);
+            this.txtIndiceLlegada.Margin = new System.Windows.Forms.Padding(4);
+            this.txtIndiceLlegada.Name = "txtIndiceLlegada";
+            this.txtIndiceLlegada.Size = new System.Drawing.Size(91, 22);
+            this.txtIndiceLlegada.TabIndex = 21;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 46);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 17);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Tiempo de Cobro";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 17);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Índice de LLegada";
+            // 
+            // gridTamaño
+            // 
+            this.gridTamaño.AllowUserToAddRows = false;
+            this.gridTamaño.AllowUserToDeleteRows = false;
+            this.gridTamaño.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridTamaño.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.gridTamaño.Location = new System.Drawing.Point(317, 13);
+            this.gridTamaño.Margin = new System.Windows.Forms.Padding(4);
+            this.gridTamaño.Name = "gridTamaño";
+            this.gridTamaño.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.gridTamaño.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.gridTamaño.Size = new System.Drawing.Size(460, 111);
+            this.gridTamaño.TabIndex = 18;
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "tamanio";
+            this.Column1.HeaderText = "Tamaño";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 60;
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "probabilidad";
+            this.Column2.HeaderText = "P";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 60;
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "acumulada";
+            this.Column3.HeaderText = "PAC";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 60;
+            // 
+            // Column4
+            // 
+            this.Column4.DataPropertyName = "inferior";
+            this.Column4.HeaderText = "INF";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 60;
+            // 
+            // Column5
+            // 
+            this.Column5.DataPropertyName = "superior";
+            this.Column5.HeaderText = "SUP";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.Width = 60;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 297);
+            this.Controls.Add(this.txtHoraInicio);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtHoraFin);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCantidadIteraciones);
+            this.Controls.Add(this.cmbCantidadCabinas);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSimular);
+            this.Controls.Add(this.gridTiempo);
+            this.Controls.Add(this.txtTiempoCobro);
+            this.Controls.Add(this.txtIndiceLlegada);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gridTamaño);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gridTiempo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridTamaño)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtHoraInicio;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.MaskedTextBox txtHoraFin;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MaskedTextBox txtCantidadIteraciones;
+        private System.Windows.Forms.ComboBox cmbCantidadCabinas;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSimular;
+        private System.Windows.Forms.DataGridView gridTiempo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.MaskedTextBox txtTiempoCobro;
+        private System.Windows.Forms.MaskedTextBox txtIndiceLlegada;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView gridTamaño;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+    }
+}
+
---
+++ tp5_2/simulacion_tp5/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace simulacion_tp5
+{
+    public partial class Form1 : Form
+    {
+        List<FilaTamanio> tamanio = new List<FilaTamanio>();
+        List<FilaTiempo> tiempo = new List<FilaTiempo>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSimular_Click(object sender, EventArgs e)
+        {
+            if (valido())
+            {
+                actualizarTipo();
+                actualizarTiempo();
+                simular();
+            }
+            else
+            {
+                MessageBox.Show("Debe Ingresar todos los datos", "Error", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool valido()
+        {
+            return txtIndiceLlegada.Text != "" && txtTiempoCobro.Text != "" && txtCantidadIteraciones.Text != "" && txtHoraInicio.Text != "" && txtHoraFin.Text != "";
+        }
+
+        private void actualizarTipo()
+        {
+            double probabilidadTotal = 0;
+            double probabilidadAcumulada = 0;
+            double probabilidadInferior = 0;
+            for (int i = 0; i < tamanio.Count; i++)
+            {
+                probabilidadTotal += tamanio[i].Probabilidad;
+            }
+            if (probabilidadTotal != 1)
+            {
+                MessageBox.Show("La Probabilidad total debe dar 1", "Probabilidad del Tipo Automovil", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                probabilidadAcumulada = 0;
+                for (int i = 0; i < tamanio.Count; i++)
+                {
+                    probabilidadAcumulada += tamanio[i].Probabilidad;
+                    tamanio[i].Acumulada = probabilidadAcumulada;
+                    tamanio[i].Inferior = probabilidadInferior;
+                    tamanio[i].Superior = probabilidadAcumulada;
+                    probabilidadInferior = probabilidadAcumulada;
+                }
+                gridTamaño.DataSource = tamanio;
+                gridTamaño.Refresh();
+            }
+        }
+
+        private void actualizarTiempo()
+        {
+            double probabilidadAcumuladaTotal = 0;
+            double probabilidadAcumulada = 0;
+            double probabilidadInferior = 0;
+            for (int i = 0; i < tiempo.Count; i++)
+            {
+                probabilidadAcumuladaTotal += tiempo[i].Probabilidad;
+            }
+            if (probabilidadAcumuladaTotal != 1)
+            {
+                MessageBox.Show("La Probabilidad Total debe ser igual a 1", "Probabilidad del Tiempo de Estacionamiento", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                probabilidadAcumulada = 0;
+                for (int i = 0; i < tiempo.Count; i++)
+                {
+                    probabilidadAcumulada += Math.Round(tiempo[i].Probabilidad, 2);
+                    tiempo[i].Acumulada = Math.Round(probabilidadAcumulada, 2);
+                    tiempo[i].Inferior = Math.Round(probabilidadInferior, 2);
+                    tiempo[i].Superior = Math.Round(probabilidadAcumulada, 2);
+                    probabilidadInferior = Math.Round(probabilidadAcumulada, 2);
+                }
+                gridTiempo.DataSource = tiempo;
+                gridTiempo.Refresh();
+            }
+        }
+
+        public void simular()
+        {
+            //
+        }
+
+
+
+    }
+}
